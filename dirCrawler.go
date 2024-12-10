13a903a103012cccdd57289a81827807481cea50
Blitz-Cloud <ionutp626@gmail.com>
2024-12-10 19:46:32 +0200

    Backup in case of disaster

---
+++ dirCrawler.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type File struct {
+	name     string
+	location string
+	content  string
+	parent   *FsNode
+}
+
+type FsNode struct {
+	name     string
+	location string
+	dirs     []*FsNode
+	files    []*File
+	parent   *FsNode
+}
+
+type Example struct {
+	Location string
+	Date     string
+	Name     string
+	Content  string
+}
+
+func Explorer(location string, node *FsNode, examples *[]Example) {
+	dirContent, err := os.ReadDir(location)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range dirContent {
+		if file.IsDir() {
+			newDirNode := FsNode{
+				name:     file.Name(),
+				location: fmt.Sprintf("%s/%s", location, file.Name()),
+				parent:   node,
+			}
+			node.dirs = append(node.dirs, &newDirNode)
+			Explorer(newDirNode.location, &newDirNode, examples)
+		} else if file.Name() == "main.c" {
+			fileLocation := fmt.Sprintf("%s/%s", location, file.Name())
+			fileContent, err := os.ReadFile(fileLocation)
+			if err != nil {
+				log.Fatal(err)
+			}
+			newFile := File{
+				name:     file.Name(),
+				location: fileLocation,
+				content:  string(fileContent),
+				parent:   node,
+			}
+			data, _ := time.Parse("2_Jan_2006", (node.parent).name)
+			newExample := Example{
+				Location: fileLocation,
+				Name:     node.name,
+				Date:     fmt.Sprintf("%d-%s-%d", data.Day(), data.Month().String(), data.Year()),
+				Content:  string(fileContent),
+			}
+			*examples = append((*examples), newExample)
+			node.files = append(node.files, &newFile)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,113 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Post struct {
+	title   string
+	data    string
+	content string
+}
+
+//	func explorer(location string) map[string]string {
+//		dir, err := os.ReadDir(location)
+//		if err != nil {
+//			os.Exit(1)
+//		}
+//		for _, file := range dir {
+//			if file.IsDir() {
+//				location := fmt.Sprintf("%s/%s", location, file.Name())
+//				explorer(location)
+//			} else if file.Name() == "main.c" {
+//				loc := fmt.Sprintf("%s/%s", location, file.Name())
+//				data, err := os.ReadFile(loc)
+//				if err != nil {
+//					os.Exit(2)
+//				}
+//				posts[location+file.Name()] = string(data)
+//			}
+//		}
+//		return posts
+//	}
+var exampleRoot FsNode
+var examples []Example
 
 func main() {
+	Explorer("/home/ionut/facultate/seminar", &exampleRoot, &examples)
+	spew.Dump(examples)
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Redirect("/login")
+	})
+
+	// inca este incompleta
+
+	app.Get("/login", func(c *fiber.Ctx) error {
+		date := c.Cookies("testC")
+		if date != "" {
+			return c.Redirect("/posts")
+		}
+		return c.Render("./pages/login.html", fiber.Map{})
+	})
+	app.Post("/login", func(c *fiber.Ctx) error {
+		data, err := url.ParseQuery(string(c.Body()))
+		if err != nil {
+			return err
+		}
+		if data["password"][0] == "h3lloId" {
+			c.Cookie(&fiber.Cookie{
+				Name:     "testC",
+				Value:    "test",
+				Expires:  time.Now().Add(7 * 24 * time.Hour),
+				HTTPOnly: true,
+			})
+		}
+		fmt.Println(data["password"][0])
+		return c.Redirect("/posts")
+	})
+
+	app.Get("/post/:day", func(c *fiber.Ctx) error {
+		day := c.Params("day")
+		examplesByDay := make([]string, len(examples))
+		for i := 0; i < len(examples); i++ {
+			if day == examples[i].Date {
+				examplesByDay = append(examplesByDay, examples[i].Name)
+			}
+		}
+		return c.Render("./pages/post.html", fiber.Map{"posts": examplesByDay})
+	})
+
+	app.Get("/posts", func(c *fiber.Ctx) error {
+		date := c.Cookies("testC")
+		if date != "" {
+			days := make([]string, len(examples))
+			for _, file := range examples {
+				ok := 1
+				for i := 0; i < len(days); i++ {
+					if days[i] == file.Date {
+						ok = 0
+					}
+				}
+				if ok == 1 {
+					days = append(days, file.Date)
+				}
+				//if day != file.name {
+				//}
+			}
+
+			spew.Dump(examples)
+			return c.Render("./pages/post.html", fiber.Map{"posts": examples})
+		} else {
+			return c.Redirect("/login")
+		}
+	})
+
+	app.Listen(":3000")
 	fmt.Printf("Hello World")
 }
