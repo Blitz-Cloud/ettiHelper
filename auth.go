19bc6f0811fa14d96ef95eb9698604fd2aede4b3
Blitz-Cloud <ionutp626@gmail.com>
2024-12-12 21:49:28 +0200

    Fix security issue that caused /post/:day/:name route to be unprotected
    How the fuck i forgot to do this

---
+++ auth.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RouteProtector(c *fiber.Ctx) error {
+	fmt.Println("Route protector hit")
+	date := c.Cookies("testC")
+	spew.Dump(date)
+	if date != "" {
+		fmt.Println("Access Allowed")
+		return c.Next()
+	}
+	return c.Redirect("/login")
+}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/mustache/v2"
 )
 
@@ -30,6 +31,8 @@ func main() {
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
+
+	app.Use(logger.New())
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Redirect("/login")
 	})
@@ -37,10 +40,6 @@ func main() {
 	// inca este incompleta
 
 	app.Get("/login", func(c *fiber.Ctx) error {
-		date := c.Cookies("testC")
-		if date != "" {
-			return c.Redirect("/posts")
-		}
 		return c.Render("login", fiber.Map{})
 	})
 	app.Post("/login", func(c *fiber.Ctx) error {
@@ -60,7 +59,8 @@ func main() {
 		return c.Redirect("/posts")
 	})
 
-	app.Get("/post/:day", func(c *fiber.Ctx) error {
+	authGroup := app.Group("/", RouteProtector)
+	authGroup.Get("/post/:day", func(c *fiber.Ctx) error {
 		day := c.Params("day")
 		examplesByDay := make([]string, len(examples))
 		for i := 0; i < len(examples); i++ {
@@ -72,29 +72,24 @@ func main() {
 		return c.Render("posts", fiber.Map{"posts": examplesByDay})
 	})
 
-	app.Get("/posts", func(c *fiber.Ctx) error {
-		date := c.Cookies("testC")
-		if date != "" {
-			days := make([]string, len(examples))
-			for _, file := range examples {
-				ok := 1
-				for i := 0; i < len(days); i++ {
-					if days[i] == file.Date {
-						ok = 0
-					}
-				}
-				if ok == 1 {
-					days = append(days, file.Date)
+	authGroup.Get("/posts", func(c *fiber.Ctx) error {
+		days := make([]string, len(examples))
+		for _, file := range examples {
+			ok := 1
+			for i := 0; i < len(days); i++ {
+				if days[i] == file.Date {
+					ok = 0
 				}
 			}
-
-			return c.Render("posts", fiber.Map{"posts": examples})
-		} else {
-			return c.Redirect("/login")
+			if ok == 1 {
+				days = append(days, file.Date)
+			}
 		}
+
+		return c.Render("posts", fiber.Map{"posts": examples})
 	})
 
-	app.Get("/post/:date/:name", func(c *fiber.Ctx) error {
+	authGroup.Get("/post/:date/:name", func(c *fiber.Ctx) error {
 		date := c.Params("date")
 		name := c.Params("name")
 		example := new(Example)
