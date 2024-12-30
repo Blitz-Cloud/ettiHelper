b9d7022d9361599eb81c51cc3fd8c5c0dccb4a0b
Blitz-Cloud <ionutp626@gmail.com>
2024-12-30 22:06:46 +0200

    Update Added preline package and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./views/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+    "node_modules/preline/dist/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require("flowbite/plugin"),
+    require("preline/plugin"),
     require("@catppuccin/tailwindcss")({
       prefix: "ctp",
       defaultFlavour: "latte",
