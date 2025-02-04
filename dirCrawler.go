5e7a5c22c5108bfb0c5d4cb0ee85186711a12c28
Blitz-Cloud <ionutp626@gmail.com>
2025-02-04 23:28:20 +0200

    Fixed and improved dirCrawler capabilities

--- dirCrawler.go
+++ dirCrawler.go
@@ -7,6 +7,8 @@ import (
 	"path"
 	"strings"
 	"time"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 type File struct {
@@ -31,7 +33,7 @@ type Example struct {
 	Content  string
 }
 
-func Explorer(location string, node *FsNode, examples *[]Example) {
+func ExplorerLegacy(location string, node *FsNode, examples *[]Example) {
 	dirContent, err := os.ReadDir(location)
 	if err != nil {
 		log.Fatal(err)
@@ -45,7 +47,7 @@ func Explorer(location string, node *FsNode, examples *[]Example) {
 				parent:   node,
 			}
 			node.dirs = append(node.dirs, &newDirNode)
-			Explorer(newDirNode.location, &newDirNode, examples)
+			ExplorerLegacy(newDirNode.location, &newDirNode, examples)
 		} else if path.Ext(file.Name()) == ".c" {
 
 			// location needed in order to read the file contents
@@ -75,5 +77,62 @@ func Explorer(location string, node *FsNode, examples *[]Example) {
 
 			node.files = append(node.files, &newFile)
 		}
+	}
+}
+
+type FactoryFunc[T any] func(*File, *[]T)
+
+func copyPasteParser(file *File, contentArray *[]Example) {
+	if file.name == "main.c" {
+
+		unParsedDate := ((file.parent).parent).name
+		date, err := time.Parse("2_Jan_2006", unParsedDate)
+		if err != nil {
+			log.Fatal(err)
+		}
+		spew.Dump(date)
+		newExample := Example{
+			Location: file.location,
+			Name:     file.name,
+			Date:     fmt.Sprintf("%d-%s-%d", date.Day(), date.Month().String()[:3], date.Year()),
+			Content:  strings.Replace(strings.Trim(string(file.content), " "), "\n", "", 1),
+		}
+		*contentArray = append((*contentArray), newExample)
+	}
+}
+
+func Explorer[T any](location string, node *FsNode, extension string, contentArray *[]T, parser FactoryFunc[T]) {
+	dirContent, err := os.ReadDir(location)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range dirContent {
+		if file.IsDir() {
+			newDirNode := FsNode{
+				name:     file.Name(),
+				location: fmt.Sprintf("%s/%s", location, file.Name()),
+				parent:   node,
+			}
+			node.dirs = append(node.dirs, &newDirNode)
+			Explorer(newDirNode.location, &newDirNode, extension, contentArray, parser)
+		} else if path.Ext(file.Name()) == extension {
+			fileLocation := fmt.Sprintf("%s/%s", location, file.Name())
+			fileContent, err := os.ReadFile(fileLocation)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			newFile := File{
+				name:     file.Name(),
+				location: fileLocation,
+				content:  string(fileContent),
+				parent:   node,
+			}
+			node.files = append(node.files, &newFile)
+			parser(&newFile, contentArray)
+		}
+
 	}
 }
