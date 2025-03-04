7242c9bed30d35e9af2d4e8da4557e419ce4fe77
Blitz-Cloud <ionutp626@gmail.com>
2025-03-04 19:11:29 +0200

    Update am comentat functii si structuri care nu mai sunt necesare

--- utils/utils.go
+++ utils/utils.go
@@ -16,17 +16,17 @@ import (
 // plus ca ar mari complexitatea in mod inutil
 // ce ar fi necesar este sa fac parsingul inante, si sa il salvez in structura
 
-func SortExamplesInDescendingOrderByDate(examples *[]Example) {
-	sort.Slice(*examples, func(i, j int) bool {
-		date1, _ := time.Parse("2-Jan-2006", (*examples)[i].Date)
-		date2, _ := time.Parse("2-Jan-2006", (*examples)[j].Date)
-		if date1.After(date2) {
-			return true
-		} else {
-			return false
-		}
-	})
-}
+// func SortExamplesInDescendingOrderByDate(examples *[]Example) {
+// 	sort.Slice(*examples, func(i, j int) bool {
+// 		date1, _ := time.Parse("2-Jan-2006", (*examples)[i].Date)
+// 		date2, _ := time.Parse("2-Jan-2006", (*examples)[j].Date)
+// 		if date1.After(date2) {
+// 			return true
+// 		} else {
+// 			return false
+// 		}
+// 	})
+// }
 
 func SortBlogPostsInDescendingOrderByDate(examples *[]BlogPost) {
 	sort.Slice(*examples, func(i, j int) bool {
