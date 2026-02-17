6052fa31b17edf061a502114dfe91858070b907a
Blitz-Cloud <ionutp626@gmail.com>
2026-02-17 21:23:35 +0200

    Update Temporary fix just for demo deployment

--- middleware/ettiAuth.go
+++ middleware/ettiAuth.go
@@ -1,13 +1,10 @@
-package routes
+package middleware
 
 import (
 	"fmt"
-	"os"
 	"strings"
-	"time"
 
 	"github.com/MicahParks/keyfunc/v3"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -17,8 +14,7 @@ var kf keyfunc.Keyfunc
 
 func InitAuth(tenantID string) {
 	// Microsoft's JWKS endpoint
-	// jwksURL := fmt.Sprintf("https://login.microsoftonline.com/%s/.well-known/openid-configuration", tenantID)
-	jwksURL := fmt.Sprintf("https://login.microsoftonline.com/upb.ro/discovery/keys")
+	jwksURL := fmt.Sprintf("https://login.microsoftonline.com/%s/discovery/v2.0/keys", tenantID)
 
 	var err error
 	// Fetch and automatically refresh keys in the background every hour
@@ -28,27 +24,21 @@ func InitAuth(tenantID string) {
 	}
 }
 
-func RegisterEttiAuth(app *fiber.App) {
-	InitAuth(os.Getenv("MS_AUTH_TENANT_ID"))
-	ettiAuth := app.Group("/api/auth/etti")
-
-	ettiAuth.Get("/", func(c *fiber.Ctx) error {
-		tenantID := os.Getenv("MS_AUTH_TENANT_ID")
-		// clientID := "cbaeae5e-cede-4a4a-ac99-63731f4679d7"
-
+func AuthMiddleware(tenantID string, clientID string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
 		authHeader := c.Get("Authorization")
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Missing token"})
 		}
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-		spew.Dump(tokenString)
 
 		// Parse and Validate
-		token, err := jwt.Parse(tokenString, kf.Keyfunc) // jwt.WithValidMethods([]string{"RS256"}), // Microsoft uses RS256
-		// jwt.WithAudience(clientID),              // Must match your App Client ID
-		// jwt.WithIssuer(fmt.Sprintf("https://login.microsoftonline.com/%s/v2.0", tenantID)),
+		token, err := jwt.Parse(tokenString, kf.Keyfunc,
+			jwt.WithValidMethods([]string{"RS256"}), // Microsoft uses RS256
+			jwt.WithAudience(clientID),              // Must match your App Client ID
+			jwt.WithIssuer(fmt.Sprintf("https://login.microsoftonline.com/%s/v2.0", tenantID)),
+		)
 
-		spew.Dump(token)
 		if err != nil || !token.Valid {
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid token"})
 		}
@@ -66,16 +56,7 @@ func RegisterEttiAuth(app *fiber.App) {
 		// Success: Store user info in context for later use in routes
 		c.Locals("user_email", claims["preferred_username"])
 		c.Locals("user_oid", claims["oid"]) // Unique ID for the user in AD
-		c.Cookie(&fiber.Cookie{
-			Name:     "testC",
-			Value:    "test",
-			Expires:  time.Now().Add(7 * 24 * time.Hour),
-			HTTPOnly: true,
-			Domain:   "api.etti.blitzcloud.me",
-			SameSite: "None",
-			Path:     "/",
-			Secure:   true,
-		})
-		return c.SendStatus(200)
-	})
+
+		return c.Next()
+	}
 }
